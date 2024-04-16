5e073587a1cc332c4ce9b4b3c4e3fe4ddb612ee1
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2024-04-16 11:42:50 +0700

    updated-questions

--- app/(router)/partner/driver/components/DriverQuestions.tsx
+++ app/(router)/partner/driver/components/DriverQuestions.tsx
@@ -14,23 +14,33 @@ const DriverQuestions = (props: Props) => {
     const listQuestions = [
         {
             id: uuidv4(),
-            title: "Kanow có xử lý cho vải thun áo thun không?",
-            description: 'Chúng tôi có dịch vụ xử lý rút tuyệt đối với 2 quy trình: giặt + sấy, trước khi cắt hàng để vải đảm bảo chất lượng hàng hoá'
+            title: "Khi xảy ra va chạm thì cách thức xử lý và chi phí sửa chữa tính như thế nào?",
+            description: 'Công ty có quy chế hỗ trợ sửa chữa khi va chạm giao thông, tùy theo nguyên nhân (chủ quan, khách quan, hỗn hợp) và tinh thần hợp tác giải quyết của Tài xế.Với sự cố nhỏ (va chạm, xước, móp…): Tài xế tự thanh toán.Với các sự cố lớn (giữ xe): Phải có Chuyên viên của bộ phận chuyên môn đi cùng để xác nhận chi phí và xử lý lấy xe ra theo từng trường hợp. Tài xế thanh toán khoản tiền theo xác nhận của Chuyên viên.'
         },
         {
             id: uuidv4(),
-            title: "Kanow có xử lý cho vải thun áo thun không?",
-            description: 'Chúng tôi có dịch vụ xử lý rút tuyệt đối với 2 quy trình: giặt + sấy, trước khi cắt hàng để vải đảm bảo chất lượng hàng hoá'
+            title: "Nếu tôi gặp tai nạn thì công ty có hỗ trợ gì không?",
+            description: 'Nếu tài xế gặp sự cố giao thông khi đang di chuyển trên đường tài xế ngay lập tức báo bộ phận chức năng để được hướng dẫn/hỗ trợ xử lý kịp thời. Đồng thời tài xế liên hệ với TT Hỗ trợ Tài xế Kanow 1900 252 228 và ĐTĐX/CVVHB xin hướng dẫn hỗ trợ nếu không tự xử lý được.'
         },
         {
             id: uuidv4(),
-            title: "Kanow có xử lý cho vải thun áo thun không?",
-            description: 'Chúng tôi có dịch vụ xử lý rút tuyệt đối với 2 quy trình: giặt + sấy, trước khi cắt hàng để vải đảm bảo chất lượng hàng hoá'
+            title: "Tôi có lương cứng không? Chính sách thưởng theo chuyến và theo năm như thế nào?",
+            description: 'Tài xế Kanow không có lương cứng. Thu nhập sẽ dựa trên doanh thu của chuyến đi và thưởng doanh thu hoạt động của tài xế. Các khoản thưởng khác sẽ dựa theo hiệu quả kinh doanh của công ty.'
         },
         {
             id: uuidv4(),
-            title: "Kanow có xử lý cho vải thun áo thun không?",
-            description: 'Chúng tôi có dịch vụ xử lý rút tuyệt đối với 2 quy trình: giặt + sấy, trước khi cắt hàng để vải đảm bảo chất lượng hàng hoá'
+            title: "Tôi có thể chia sẻ tài khoản chạy chung với người khác được không?",
+            description: 'Tuyệt đối không chia sẻ tài khoản cho người khác chạy chung, nếu tài xế có hành vi chia sẻ tài khoản cho người khác sẽ bị khóa tài khoản và bị ngừng hợp tác vĩnh viễn'
+        },
+        {
+            id: uuidv4(),
+            title: "Tôi mới thi bằng lái xe có giấy hẹn thì có thể đăng ký chạy xe không?",
+            description: 'Tài xế đăng ký tài khoản Kanow bắt buộc mang toàn bộ giấy tờ bản gốc để nhân viên kiểm tra và đăng ký tài khoản. Kanow sẽ không thu giữ giấy tờ cá nhân nào của tài xế sau đó.'
+        },
+        {
+            id: uuidv4(),
+            title: "TX đã nạp tiền nhưng ví trên chưa nhận được hoặc KH đã thanh toán nhưng ví dưới chưa nhận được",
+            description: 'TX vui lòng kiểm tra lại với ngân hàng. Trường hợp đã kiểm tra vẫn chưa được vui lòng báo về TT Hỗ trợ Tài xế Kanow 1900 252 228 để được hỗ trợ.'
         },
     ]
 
